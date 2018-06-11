600be05a52ff52033310015b526596b43d84062d
20172104244 <1060071702@qq.com>
2018-06-11 16:29:26 +0800

    CFeet-CMyfeet
    完善了以前写的。弄清多态与虚函数的关系。还知道了(->)和(.)的作用是一样的。无名对象是何物。

--- CFeet-CMyfeet/CFeet-CMyfeet.cpp
+++ CFeet-CMyfeet/CFeet-CMyfeet.cpp
@@ -11,10 +11,11 @@ protected:
 	int inches;
 public:
 	CFeet();
-	void setfeet(int ft);
+	void setfeet(int ft);//易调用
 	void setinches(int ich);
-	virtual void display();//变成虚函数
-	int getfeet();
+	virtual void display();//virtual只能在类定义中的成员函数说明处使用，通过基累指针调用基类和派生类中都有的同名，同参数的叫虚函数。
+	//所以要用virtual变成虚函数
+	int getfeet();//可访问
 	int getinches();
 };
 CFeet::CFeet()
@@ -35,8 +36,31 @@ void CFeet::display()
 {
 	cout << "基类  " << feet << "英尺" << inches << "英寸" << endl;
 }
+int CFeet::getfeet()
+{
+	return feet;
+}
+int CFeet::getinches()
+{
+	return inches;
+}
+class CMyfeet :public CFeet
+{
+public:
+	void display();
+};
+void CMyfeet::display()
+{
+	cout << "派生类  " << feet << "英尺" << inches << "英寸" << endl;
+}
 int main()
 {
+	CFeet *p;
+	p = new CMyfeet;//把CMyfeet变成了无名对象。是跟CMyfeet Afeet; p=&Afeet;一样的
+	p->setfeet(3);// （->)的作用是跟(.)一样的
+	p->setinches(8);
+	p->display();//派生类
+	p->CFeet::display();//基类
     return 0;
 }
 
